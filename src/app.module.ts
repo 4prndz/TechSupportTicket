d32e1e8166d29ec8eafb9176d73bd02292e8f00e
4prndz <victorkouichi40@gmail.com>
2024-03-24 03:33:36 -0300

    feat: better logging with pino

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
+import { LoggerModule } from 'nestjs-pino';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TicketModule } from './ticket/ticket.module';
-import { TicketModule } from './ticket/ticket.module';
 
 @Module({
-  imports: [TicketModule],
+  imports: [
+    TicketModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport: {
+          target: 'pino-pretty',
+          options: {
+            singleLine: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from 'nestjs-pino';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -12,7 +13,9 @@ async function bootstrap() {
     .addTag('tickets', 'Endpoints related to ticket management')
     .addTag('auth', 'Endpoints for user authentication')
     .build();
+  app.useLogger(app.get(Logger));
   const document = SwaggerModule.createDocument(app, config);
+
   SwaggerModule.setup('api', app, document);
 
   await app.listen(3000);
